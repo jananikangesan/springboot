bfd6573effd23c8636d300b0a65ef618267ea7fb
jananikangesan <janakanges1996@gmail.com>
2023-09-26 10:02:46 +0530

    Dependency Injection

--- src/main/java/com/example/demo/model/StudentService.java
+++ src/main/java/com/example/demo/model/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.model;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents(){
         return List.of(
