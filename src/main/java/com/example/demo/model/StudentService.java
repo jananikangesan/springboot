f2212691fe3f34c728a02d0edbe675333358e416
jananikangesan <janakanges1996@gmail.com>
2023-09-26 09:59:12 +0530

    Business Layer

---
+++ src/main/java/com/example/demo/model/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "abc",
+                        "abc@gamil.com",
+                        LocalDate.of(2000, Month.JANUARY,5),
+                        21
+                )
+        );
+    }
+}
